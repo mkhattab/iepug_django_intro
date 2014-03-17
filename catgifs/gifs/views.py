31f0c924f995f54b702c01c8467771d7eb5f244d
Mustafa Khattab <mustafa.khattab@gmail.com>
2014-03-17 13:13:25 -0700

    Add cat GIFs index view

--- catgifs/gifs/views.py
+++ catgifs/gifs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import GIF
+
+
+def index(request):
+    qs = GIF.objects.order_by('?')
+    gif = qs.first()
+
+    response = '''
+    <h1>{}</h1>
+    <img src={}>
+    '''.format(gif.description, gif.url)
+
+    return HttpResponse(response)
