291fa237a34b001e2b05963265e1512481d89948
Mustafa Khattab <mustafa.khattab@gmail.com>
2014-03-17 12:55:10 -0700

    Add GIF model

--- catgifs/gifs/models.py
+++ catgifs/gifs/models.py
@@ -1,3 +1,11 @@
+import os
+
 from django.db import models
 
-# Create your models here.
+class GIF(models.Model):
+    url = models.URLField(unique=True)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return os.path.basename(self.url)
